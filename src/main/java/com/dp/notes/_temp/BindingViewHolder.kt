4dfb0261bbfc2eb20f2d88567ab58e94efa92f83
dengqiang <q.deng@liaoke.tv>
2023-04-11 17:52:06 +0800

    BaseQuickAdapter 添加绑定biewbinding的Viewholder

---
+++ src/main/java/com/dp/notes/_temp/BindingViewHolder.kt
@@ -0,0 +1,11 @@
+package com.dp.notes._temp
+
+/**
+ * author Dq
+ * date on 2023/4/11
+ * description
+ */
+//class BindingViewHolder<T : ViewBinding>(val binding: T) : BaseViewHolder(binding.root)
+//
+//val ViewGroup.inflater get() = LayoutInflater.from(this.context)
+
